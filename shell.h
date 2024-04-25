7fb93b8224ca8e4f23a316798678f31467c61c94
Jose Mayo <josemayo3@gmail.com>
2024-04-25 15:23:29 -0400

    push header file test 1

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H_
+#define SHELL_H_
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int receiveCommand(void);
+
+
+#endif
