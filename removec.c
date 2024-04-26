0d2e585731200029ee24c37597675524e769d7a3
Jose Mayo <josemayo3@gmail.com>
2024-04-26 09:39:51 -0400

    Update removec.c

--- removec.c
+++ removec.c
@@ -1,1 +1,17 @@
+#include "shell.h"
 
+/**
+ * removetrash - function to remove the newline character from user input
+ * @str: user input
+ * @len: lenght of the user input
+ * 
+ * Return: returns the command without a newline character
+*/
+
+char *removetrash(char *str, int len)
+{
+    if (str != 0 && str[len - 1] == '\n')
+        str[len - 1] = '\0';
+
+    return (str);
+}
