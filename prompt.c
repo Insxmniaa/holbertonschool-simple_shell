4be2017d3791ae33fa5bf104c2a562c5b1f0e810
Jose Mayo <josemayo3@gmail.com>
2024-04-25 20:31:57 -0400

    update prompt.c

--- prompt.c
+++ prompt.c
@@ -1,31 +1,37 @@
 #include "shell.h"
 
 /**
- * receiveCommand - takes user input
+ * getinput - takes user input
  * 
  * Return: 0
 */
 
-int receiveCommand(void)
+int getinput(void)
 {
     size_t n = 0;
-    char *buff = NULL;
-    int size = getline(&buff, &n, stdin); /*getline waits the user to enter something in terminal*/
-
+    char *buff = NULL, *command, *argsC[MAX_ARG];
+    int size, argcount; 
+    
+    size = getline(&buff, &n, stdin); /*getline waits the user to enter something in terminal*/
     if (size == -1)
     {
-        if (feof(stdin))
-        {
-            free(buff);
-            exit(0);
-        }
-
-        else
-        {
-            perror("No user input");
-            free(buff);
-        }
-
+            if (feof(stdin))
+            {
+                free(buff);
+                exit(0);
+            }
+            else
+            {
+                ("Failed to read user input");
+                free(buff);
+            }
     }
+
+    command = removetrash(buff, size);
+    argcount = tokenize(command, argsC);
+
+    executecommand(argsC);
+
+    free(buff);
     return (size);
 }
