8f2a396ca0213b0416c6f56463f361ae17b4f179
Insxmniaa <jeanne.melendez@icloud.com>
2024-04-25 23:50:56 -0400

    env

--- shell.h
+++ shell.h
@@ -7,7 +7,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <errno.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <signal.h>
 
 #define MAX_INPUT 1024
 #define MAX_ARGS 100
@@ -15,6 +18,10 @@
 void getinput();
 void execute_command(char **args);
 char **parse_input(char *input_line);
+void env(char **arv);
+void _setenv(char **arv);
+void _unsetenv(char **arv);
+char *_getenv(const char *name);
 
 
 #endif
