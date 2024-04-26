a2600a2ef036587eaaea25dda7a37bdcff14d140
Jose Mayo <josemayo3@gmail.com>
2024-04-26 09:37:13 -0400

    Update prompt.c

--- prompt.c
+++ prompt.c
@@ -1,13 +1,37 @@
 #include "shell.h"
 
 /**
- * print_prompt - prints the prompt in the shell
+ * getinput - takes user input
  * 
- * Return: the prompt
+ * Return: 0
 */
 
-void print_prompt() 
+int getinput(void)
 {
-    printf("#cisfun$ ");
-    fflush(stdout);
+    size_t n = 0;
+    char *buff = NULL, *command, *argsC[MAX_ARG];
+    int size; 
+    
+    size = getline(&buff, &n, stdin); /*getline waits the user to enter something in terminal*/
+    if (size == -1)
+    {
+            if (feof(stdin))
+            {
+                free(buff);
+                exit(0);
+            }
+            else
+            {
+                printf("Failed to read user input");
+                free(buff);
+            }
+    }
+
+    command = removetrash(buff, size);
+    tokenize(command, argsC);
+
+    executecommand(argsC);
+
+    free(buff);
+    return (size);
 }
