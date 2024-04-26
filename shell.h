ee05d23ac1eb53f3dfa7fd52213a107bc8034bfa
Jose Mayo <josemayo3@gmail.com>
2024-04-25 20:34:07 -0400

    updated header file

--- shell.h
+++ shell.h
@@ -5,9 +5,19 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-int receiveCommand(void);
+
+#define MAX_ARG 25
+
+int main(void);
+int getinput(void);
+char *removetrash(char *str, int len);
+int tokenize(char *command, char *argsC[]);
+int executecommand(char *argsC[]);
+
+
 
 
 #endif
