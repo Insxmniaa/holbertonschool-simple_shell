05009f6b86e01874e9934f84649e24e4390a4457
Alex <alexccv94@gmail.com>
2024-04-26 08:04:12 -0400

    a couple of fixes

--- path.c
+++ path.c
@@ -6,50 +6,57 @@
  *
  *cmd_exists = function to check if a command exists in a directory
  *exc_command = function to exectute command if it exists
- *
+ *cmd = the command to execute
  */
 
-int cmd_exists(const char * cmd, const char *directory, char *path)
+int cmd_exists(const char *cmd, const char *directory, const char *path)
 {
-	{
-		char full_path[MAX_PATH_LENGTH];
-		snprintf(full_path, MAX_PATH_LENGTH, "%s/%s", directory, cmd);
-		return access(full_path, F_OK) == 0;
-	}
+	char full_path[MAX_PATH_LENGTH];
+
+	snprintf(full_path, MAX_PATH_LENGTH, "%s/%s", directory, cmd);
+	return access(full_path, F_OK) == 0;
+}
 
 void exc_command(const char *cmd)
+{
+	char *path = getenv("PATH");
+
+	if (!path)
 	{
-		char *path = getenv("PATH");
-		if(!path)
-		fprintf(stderr, "PATH enviroment variable not found|\n");
+		fprintf(stderr, "PATH environment variable not found\n");
 		exit(1);
 	}
 
-	char *dir = strtok(path, ":");
-	while (dir != NULL)
+	char *token;
+	char *path_copy = strdup(path);
+	token = strtok(path_copy, ":");
+	while (token != NULL)
 	{
-	if (cmd_exists(cmd, dir, path))
-	{
-		pid_t pid = fork();
-		if (pid == -1)
+		if (cmd_exists(cmd, token, cmd))
+		{
+			pid_t pid = fork();
+			if (pid == -1)
 			{
 				perror("fork");
 				exit(1);
 			}
-		else if (pid == 0)
-		{
-			execlp(cmd, cmd, NULL);
-			perror("execlp");
-			exit(1);
+			else if (pid == 0)
+			{
+				char full_path[MAX_PATH_LENGTH];
+				snprintf(full_path, MAX_PATH_LENGTH, "%s/%s", token, cmd);
+				execlp(full_path, cmd, NULL);
+				perror("execlp");
+				exit(1);
+			}
+			else
+			{
+				wait(NULL);
+				return;
+			}
 		}
-		else
-		{
-			wait(NULL);
-			exit(0);
-		}
+		token = strtok(NULL, ":");
 	}
-	dir = strtok(NULL, ":");
-}
-	fprintf(stderr,"%s: command not found\n", cmd);
+
+	fprintf(stderr, "%s: command not found\n", cmd);
 	exit(1);
 }
