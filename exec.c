1010b29038bb5864a2317b4567384311cbde139d
Jose Mayo <josemayo3@gmail.com>
2024-04-26 09:39:10 -0400

    Update exec.c

--- exec.c
+++ exec.c
@@ -1,1 +1,36 @@
+#include "shell.h"
 
+/**
+ * executecommand - function to execute the user command
+ * @argsC array of commands from the user
+ * 
+ * Return: O on success
+*/
+
+int executecommand(char *argsC[])
+{
+    pid_t my_pid;
+    int execmd, status;
+
+    my_pid = fork();
+
+    if (my_pid == -1)
+    {
+            perror("Process creation failure");
+    }
+    else if (my_pid == 0)
+    {
+            execmd = execve(argsC[0], argsC, NULL);
+            
+            if (execmd == -1)
+            {
+                    perror("Error executing command");
+            }
+
+    }
+    else 
+        wait(&status);
+    
+    return (0);
+
+}
