94854b8d9c0ec49141b7b8f22e27a6671ea9fe7f
Alexcc20 <123332944+Alexcc20@users.noreply.github.com>
2024-04-26 09:45:57 -0400

    Update path.c

--- path.c
+++ path.c
@@ -8,55 +8,53 @@
  *exc_command = function to exectute command if it exists
  *cmd = the command to execute
  */
-
-int cmd_exists(const char *cmd, const char *directory, const char *path)
+int cmd_exists(const char *cmd, const char *directory)
 {
-	char full_path[MAX_PATH_LENGTH];
-
-	snprintf(full_path, MAX_PATH_LENGTH, "%s/%s", directory, cmd);
-	return access(full_path, F_OK) == 0;
+    char full_path[MAX_PATH_LENGTH];
+    snprintf(full_path, MAX_PATH_LENGTH, "%s/%s", directory, cmd);
+    return access(full_path, F_OK) == 0;
 }
 
 void exc_command(const char *cmd)
 {
-	char *path = getenv("PATH");
+    char *path = getenv("PATH");
 
-	if (!path)
-	{
-		fprintf(stderr, "PATH environment variable not found\n");
-		exit(1);
-	}
+    if (!path)
+    {
+        fprintf(stderr, "PATH environment variable not found\n");
+        exit(1);
+    }
 
-	char *token;
-	char *path_copy = strdup(path);
-	token = strtok(path_copy, ":");
-	while (token != NULL)
-	{
-		if (cmd_exists(cmd, token, cmd))
-		{
-			pid_t pid = fork();
-			if (pid == -1)
-			{
-				perror("fork");
-				exit(1);
-			}
-			else if (pid == 0)
-			{
-				char full_path[MAX_PATH_LENGTH];
-				snprintf(full_path, MAX_PATH_LENGTH, "%s/%s", token, cmd);
-				execlp(full_path, cmd, NULL);
-				perror("execlp");
-				exit(1);
-			}
-			else
-			{
-				wait(NULL);
-				return;
-			}
-		}
-		token = strtok(NULL, ":");
-	}
+    char *token;
+    char *path_copy = strdup(path);
+    token = strtok(path_copy, ":");
+    while (token != NULL)
+    {
+        if (cmd_exists(cmd, token))
+        {
+            pid_t pid = fork();
+            if (pid == -1)
+            {
+                perror("fork");
+                exit(1);
+            }
+            else if (pid == 0)
+            {
+                char full_path[MAX_PATH_LENGTH];
+                snprintf(full_path, MAX_PATH_LENGTH, "%s/%s", token, cmd);
+                execlp(full_path, cmd, NULL);
+                perror("execlp");
+                exit(1);
+            }
+            else
+            {
+                wait(NULL);
+                return;
+            }
+        }
+        token = strtok(NULL, ":");
+    }
 
-	fprintf(stderr, "%s: command not found\n", cmd);
-	exit(1);
+    fprintf(stderr, "%s: command not found\n", cmd);
+    exit(1);
 }
