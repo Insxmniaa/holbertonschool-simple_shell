d1eb83834a20fb9d6a8b0616883462e124165df2
Alex <alexccv94@gmail.com>
2024-04-26 23:11:29 -0400

    test

---
+++ get_command.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * get_command - find the path to the command in the input given
+ *
+ * @cmd: is the args[0] in main function
+ *
+ * Return: full path if successful, NULL if failed
+*/
+char *get_command(char *cmd)
+{
+	static char *locations[] = {"/bin/", "/usr/bin/", NULL};
+	int i;
+	char *full_path;
+
+	if (access(cmd, X_OK) == 0)
+	{
+		full_path = malloc(strlen(cmd) + 1);
+		strcpy(full_path, cmd);
+		return (full_path);
+	}
+
+	for (i = 0; locations[i] != NULL; i++)
+	{
+		full_path = malloc(strlen(locations[i]) + strlen(cmd) + 1);
+		if (full_path == NULL)
+		{
+			perror("Failed allocating memory");
+			exit(EXIT_FAILURE);
+		}
+
+		sprintf(full_path, "%s%s", locations[i], cmd);
+
+		if (access(full_path, X_OK) == 0)
+		{
+			return (full_path);
+		}
+
+		free(full_path);
+	}
+	return (NULL);
+}
---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H 
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+
+#define MAX_ARGS 64
+
+char* get_command(char *cmd);
+
+extern char **environ;
+
+
+#endif
